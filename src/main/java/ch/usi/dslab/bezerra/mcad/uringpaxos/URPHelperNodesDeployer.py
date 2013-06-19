acf268813593e0ad07420af26961d109eafaede3
Eduardo Bezerra <carlos.bezerra@usi.ch>
2013-06-19 14:09:41 +0200

    Added the urpmcast common configuration example to the urpmcast implementation folder.

--- src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/URPHelperNodesDeployer.py
+++ src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/URPHelperNodesDeployer.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import json
 from pprint import pprint
@@ -6,3 +7,19 @@ system_config_file = sys.argv[1]
 
 print("Deploying system helper nodes described in " + system_config_file)
 
+config_json = open(system_config_file)
+
+config = json.load(config_json)
+
+
+
+# (re)start zookeeper
+zookeeper_server_location = config["zookeeper"]["location"]
+zookeeper_server_address  = zookeeper_server_location + ":" + config["zookeeper"]["port"]
+
+print("zkLocation: " + zookeeper_server_location + "; zkAddress: " + zookeeper_server_address)
+
+
+
+
+json_data.close()
