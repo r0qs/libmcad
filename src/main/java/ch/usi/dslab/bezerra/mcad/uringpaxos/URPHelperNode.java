0ad6c31922e6911c2d71b7e55f4bc6bfc9476936
Eduardo Bezerra <carlos.bezerra@usi.ch>
2013-06-19 16:21:37 +0200

    Finished the urp deployer python script. Started developing the urp node.

--- src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/URPHelperNode.java
+++ src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/URPHelperNode.java
@@ -1,13 +1,122 @@
 package ch.usi.dslab.bezerra.mcad.uringpaxos;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import org.apache.log4j.Logger;
+
+import ch.usi.da.paxos.api.PaxosNode;
+import ch.usi.da.paxos.examples.TTYNode;
+import ch.usi.da.paxos.ring.RingDescription;
+
 public class URPHelperNode {
 
-   /**
-    * @param args
-    */
-   public static void main(String[] args) {
-      // TODO Auto-generated method stub
+   static Logger logger = Logger.getLogger(TTYNode.class);
 
+   private static class HelperProposer implements Runnable {
+      PaxosNode paxos;
+      BlockingQueue<byte[]> pendingMessages;
+      ServerSocket listeningSocket;
+      boolean running = true;
+      ConnectionListener connectionListener;
+
+      public HelperProposer(PaxosNode paxos, int port) {
+         try {
+            this.paxos = paxos;
+            pendingMessages = new LinkedBlockingQueue<byte[]>();
+            listeningSocket = new ServerSocket(port);
+            connectionListener = new ConnectionListener(this);
+            Thread t = new Thread(connectionListener);
+            t.start();
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      }
+
+      @Override
+      public void run() {
+         try {
+            while (running) {
+               byte[] proposal = pendingMessages.take();
+               for (RingDescription ring : paxos.getRings()) {
+                     paxos.getProposer(ring.getRingID()).propose(proposal);
+               }
+            }
+            connectionListener.running = false;
+         } catch (InterruptedException e) {
+            logger.error(e);
+         }
+      }
+   }
+   
+   private static class ConnectionListener implements Runnable {
+      HelperProposer helperProposer;
+      ArrayList<ProposerConnection> connectionList;
+      boolean running = true;
+      
+      public ConnectionListener(HelperProposer hp) {
+         this.helperProposer = hp;
+         connectionList = new ArrayList<ProposerConnection>();
+      }
+
+      @Override
+      public void run() {
+         try {
+            while (running) {
+               Socket connectionSocket = helperProposer.listeningSocket.accept();
+               ProposerConnection newconnection = new ProposerConnection(helperProposer, connectionSocket);
+               connectionList.add(newconnection);
+               Thread connthread = new Thread(newconnection);
+               connthread.start();
+            }
+            for (ProposerConnection p : connectionList) {
+               p.running = false;
+            }
+            
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      }      
+   }
+   
+   private static class ProposerConnection implements Runnable {
+      HelperProposer helperProposer;
+      Socket connectionSocket;
+      boolean running = true;
+      
+      public ProposerConnection(HelperProposer hp, Socket connectionSocket) {
+         this.helperProposer = hp;
+         this.connectionSocket = connectionSocket;
+      }
+      
+      @Override
+      public void run() {
+         InputStream is;
+         ObjectInputStream ois;
+         try {
+            is = connectionSocket.getInputStream();
+            ois = new ObjectInputStream(is);
+            
+            while (running) {
+               byte[] new_message = (byte[]) ois.readObject();
+               helperProposer.pendingMessages.add(new_message);
+            }
+            
+         } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+         }
+         
+      }
+      
    }
 
+   public static void main (String args[]) {
+      
+   }
 }
--- src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/URPHelperNodesDeployer.py
+++ src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/URPHelperNodesDeployer.py
@@ -15,11 +15,28 @@ config = json.load(config_json)
 
 # (re)start zookeeper
 zookeeper_server_location = config["zookeeper"]["location"]
-zookeeper_server_address  = zookeeper_server_location + ":" + config["zookeeper"]["port"]
+zookeeper_server_port = str(config["zookeeper"]["port"])
+zookeeper_path = config["zookeeper"]["path"]
+zookeeper_server_address  = zookeeper_server_location + ":" + zookeeper_server_port
 
-print("zkLocation: " + zookeeper_server_location + "; zkAddress: " + zookeeper_server_address)
+print("[re]starting zookeeper server at " + zookeeper_server_address)
+os.system("ssh " + zookeeper_server_location + " " + zookeeper_path + " start")
+
+for node in config["helper_nodes"] :
+    nodestring = ""    
+    for ring in node["node_rings"] :
+        if len(nodestring) > 0 : nodestring += ";"
+        nodestring += str(ring["ring_id"]) + "," + str(node["node_id"]) + ":"
+        if "acceptor" in ring["roles"] : nodestring += "A"
+        if "proposer" in ring["roles"] : nodestring += "P"
+        if "learner"  in ring["roles"] : nodestring += "L"
+    
+    node_location = node["node_location"]
+    command_string = "ssh " + node_location + " (path to URPHelperNode class) " + nodestring + " " + zookeeper_server_address
+    print(command_string)
+#     os.system(command_string)
 
 
 
 
-json_data.close()
+config_json.close()
