6b4d1583ba6411f6d39746c78cd5b75f4766b339
Eduardo Bezerra <carlos.bezerra@usi.ch>
2013-08-26 20:03:15 -0400

    Added a Message to class to make it easier to send and receive virtually any kind of data.

---
+++ src/main/java/ch/usi/dslab/bezerra/mcad/Message.java
@@ -0,0 +1,73 @@
+package ch.usi.dslab.bezerra.mcad;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class Message {
+   ArrayList<Object> contents;
+
+   int next = 0;
+
+   public Message(Object... objs) {
+      contents = new ArrayList<Object>(objs.length);
+      for (Object o : objs)
+         contents.add(o);
+   }
+
+   public Object getNext() {
+      return get(next++);
+   }
+
+   public Object get(int index) {
+      if (index >= contents.size())
+         return null;
+      return contents.get(index);
+   }
+
+   public byte[] getBytes() {
+      byte[] bytes = null;
+      ByteArrayOutputStream bos = new ByteArrayOutputStream();
+      ObjectOutput out = null;
+      try {
+         out = new ObjectOutputStream(bos);
+         out.writeObject(this);
+         bytes = bos.toByteArray();
+      } catch (IOException e) {
+         e.printStackTrace();
+      } finally {
+         try {
+            out.close();
+            bos.close();
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      }
+      return bytes;
+   }
+
+   public static Message createFromBytes(byte[] bytes) {
+      Message msg = null;
+      ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+      ObjectInput in = null;
+      try {
+         in = new ObjectInputStream(bis);
+         msg = (Message) in.readObject();
+      } catch (IOException | ClassNotFoundException e) {
+         e.printStackTrace();
+      } finally {
+         try {
+            bis.close();
+            in.close();
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      }
+      return msg;
+   }
+}
