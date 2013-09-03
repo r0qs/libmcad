fc002323bfd0689ddef15676fbdf55dc94dc217f
Eduardo Bezerra <carlos.bezerra@usi.ch>
2013-09-03 16:19:51 -0400

    purge_rings.sh now kills mrp processes in remote locations too.

--- src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/purge_rings.sh
+++ src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/purge_rings.sh
@@ -2,6 +2,11 @@
 
 echo -n "Purging all java processes... "
 killall -9 java
+for i in `seq $1 $2`
+do
+   echo -n "in node$i... "
+   ssh node$i killall -9 java
+done
 sleep 1
 echo "[done]"
 $HOME/zoo/bin/zkServer.sh start
