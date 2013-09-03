b62b9991c2498f36cebab6bd6b3ceb89f4c8c627
Eduardo Bezerra <carlos.bezerra@usi.ch>
2013-09-03 23:17:31 +0200

    purge_rings now restarts zk in given node number.

--- src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/purge_rings.sh
+++ src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/purge_rings.sh
@@ -10,6 +10,7 @@ done
 sleep 1
 echo "[done]"
 $HOME/zoo/bin/zkServer.sh start
+ssh node$3 $HOME/zoo/bin/zkServer.sh start
 echo -n "Waiting for zookeeper's ephemeral nodes to vanish..."
 sleep 4
 echo " [done]"
