dc59bba057301198be38503cd560727a8d6d818f
Eduardo Bezerra <carlos.bezerra@usi.ch>
2013-09-25 14:45:22 -0400

    Size threshold = 0, i.e., no batching.

--- src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/URPHelperNode.java
+++ src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/URPHelperNode.java
@@ -56,7 +56,8 @@ public class URPHelperNode {
       @Override
       public void run() {
 //         int sizeBatchThreshold = 32000; // 32k, discounting overheads (so it's not 32768)
-         int sizeBatchThreshold = 16384; // 16k, to avoid "Buffer too small" of umrpaxos
+//         int sizeBatchThreshold = 16384; // 16k, to avoid "Buffer too small" of umrpaxos
+         int sizeBatchThreshold = 0; // 0 == no batching
          int timeBatchThreshold  = 10;   // 10 milliseconds
          long lastBatchTime = System.currentTimeMillis();
          Message batch = new Message();
