39d4de37dac06d85d5c727c7050d0aa1ff68e3c3
Eduardo Bezerra <carlos.bezerra@usi.ch>
2013-10-08 19:00:48 -0400

    Changed ArrayList to List in mcagent api.

--- src/main/java/ch/usi/dslab/bezerra/mcad/MulticastAgent.java
+++ src/main/java/ch/usi/dslab/bezerra/mcad/MulticastAgent.java
@@ -1,10 +1,10 @@
 package ch.usi.dslab.bezerra.mcad;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface MulticastAgent {
    public void multicast(Group single_destination, byte [] message);
-   public void multicast(ArrayList<Group> destinations, byte [] message);
+   public void multicast(List<Group> destinations, byte [] message);
    public byte [] deliver();
    public Message deliverMessage();
    
