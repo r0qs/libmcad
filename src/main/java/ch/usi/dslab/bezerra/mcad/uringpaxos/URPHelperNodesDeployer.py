c0ab18974e2a207f7f4bc430de6dec42992b46ba
Eduardo Bezerra <carlos.bezerra@usi.ch>
2013-10-10 17:49:39 -0400

    No message given for this commit.

--- src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/URPHelperNodesDeployer.py
+++ src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/URPHelperNodesDeployer.py
@@ -22,10 +22,11 @@ class launcherThread (threading.Thread):
         threading.Thread.__init__(self)
         self.cmdList = clist
     def run(self):
+        sleep(1)
         for cmd in self.cmdList :
-            print "xXx Executing " + cmd
+            print "xXx executing: " + cmd
             os.system(cmd);
-            sleep(1)
+            sleep(0.5)
 
 #====================================
 #====================================
