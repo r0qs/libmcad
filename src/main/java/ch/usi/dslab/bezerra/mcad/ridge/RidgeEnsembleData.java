5deae79d762b13b8aac05c45dbc0ab09e9af7f22
Eduardo Bezerra <carlos.bezerra@usi.ch>
2014-04-03 18:28:59 +0200

    Working on RidgeMulticastAgent.

--- src/main/java/ch/usi/dslab/bezerra/mcad/ridge/RidgeEnsembleData.java
+++ src/main/java/ch/usi/dslab/bezerra/mcad/ridge/RidgeEnsembleData.java
@@ -1,73 +1,49 @@
 package ch.usi.dslab.bezerra.mcad.ridge;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
 
+import ch.usi.dslab.bezerra.ridge.Ensemble;
+
 public class RidgeEnsembleData {
-   static ArrayList<RidgeEnsembleData> ringsList;
+   static ArrayList<RidgeEnsembleData> ensemblesList;
    private final static Logger log = Logger.getLogger(RidgeEnsembleData.class);
-   
+
+   int ensembleId;
    ArrayList<RidgeGroup> destinationGroups;
    
-   int    ringId;
-   String coordinatorAddress;
-   int    coordinatorPort;
-   
-   SocketChannel coordinatorConnection;
+   //TODO : set this value
+   Ensemble ensemble;
    
    static {
-      ringsList = new ArrayList<RidgeEnsembleData>();
+      ensemblesList = new ArrayList<RidgeEnsembleData>();
    }
-   
+
    static RidgeEnsembleData getById(int id) {
-      for (RidgeEnsembleData urd : ringsList)
-         if (urd.getId() == id)
-            return urd;
+      for (RidgeEnsembleData red : ensemblesList)
+         if (red.getId() == id)
+            return red;
       return null;
    }
-   
-   public RidgeEnsembleData (int ringId) {
-      this.ringId = ringId;
-      ringsList.add(this);
+
+   public RidgeEnsembleData (int ensembleId) {
+      this.ensembleId = ensembleId;
+      ensemblesList.add(this);
       destinationGroups = new ArrayList<RidgeGroup>();
    }
-   
-   public void setCoordinator(String address, int port) {
-      coordinatorAddress = address;
-      coordinatorPort    = port;
-   }
-   
-   public void connectToCoordinator() {
-      try {
-         coordinatorConnection = SocketChannel.open();
-         coordinatorConnection.connect(new InetSocketAddress(coordinatorAddress, coordinatorPort));
-      } catch (IOException e) {
-         log.fatal(" !!! Couldn't connect to ring " + this.ringId);
-         e.printStackTrace();
-         System.exit(1);
-      }
-      
-   }
-   
+
    public int getId() {
-      return ringId;
-   }
-   
-   public String getProposerAddress() {
-      return coordinatorAddress;
-   }
-   
-   public int getProposerPort() {
-      return coordinatorPort;
+      return ensembleId;
    }
    
    public void addDestinationGroup(RidgeGroup g) {
       if (destinationGroups.contains(g) == false)
          destinationGroups.add(g);
+   }
+   
+   public Ensemble getEnsemble() {
+      return ensemble;
    }
 
 }
