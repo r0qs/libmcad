084c226763cfa7af6d80b9bcf4d3bcffbb270ab0
Eduardo Bezerra <carlos.bezerra@usi.ch>
2014-04-09 20:08:12 +0200

    Created PrintDeliverInterface for debugging purposes.

--- src/main/java/ch/usi/dslab/bezerra/mcad/ridge/RidgeEnsembleNode.java
+++ src/main/java/ch/usi/dslab/bezerra/mcad/ridge/RidgeEnsembleNode.java
@@ -1,8 +1,33 @@
 package ch.usi.dslab.bezerra.mcad.ridge;
 
+import com.sun.media.jfxmedia.logging.Logger;
+
 import ch.usi.dslab.bezerra.ridge.Coordinator;
+import ch.usi.dslab.bezerra.ridge.DeliverInterface;
+import ch.usi.dslab.bezerra.ridge.Learner;
+import ch.usi.dslab.bezerra.ridge.RidgeMessage;
 
 public class RidgeEnsembleNode {
+   
+   public static class PrintDeliverInterface implements DeliverInterface {
+
+      @Override
+      public void deliverConservatively(RidgeMessage message) {
+         System.out.println(String.format("PrintDeliverInterface: Delivered message %s conservatively", message.getId()));
+      }
+
+      @Override
+      public void deliverOptimistically(RidgeMessage message) {
+         System.out.println(String.format("PrintDeliverInterface: Delivered message %s optmistically", message.getId()));         
+      }
+
+      @Override
+      public void deliverFast(RidgeMessage message) {
+         System.out.println(String.format("PrintDeliverInterface: Delivered message %s fast", message.getId()));
+      }
+      
+   }
+   
    ch.usi.dslab.bezerra.ridge.Process ridgeNode;
    int pid;
    
@@ -15,6 +40,8 @@ public class RidgeEnsembleNode {
    public void startRunning() {
       if (ridgeNode instanceof Coordinator)
          System.out.println(String.format("Coordinator %d starting...", pid));
+      else if (ridgeNode instanceof Learner)
+         ((Learner) ridgeNode).setDeliverInterface(new PrintDeliverInterface());
       ridgeNode.startRunning();
    }
    
