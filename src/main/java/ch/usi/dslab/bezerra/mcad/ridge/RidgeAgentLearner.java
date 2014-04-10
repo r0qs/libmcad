8377fb7909b0335e15c4eac242b0f5e2dc5d0fe7
Eduardo Bezerra <carlos.bezerra@usi.ch>
2014-04-10 11:40:58 +0200

    Separated the delivery callbacks from the ridge layer into a separate class that distributes the deliveries in different queues for the mcast adaptor.

---
+++ src/main/java/ch/usi/dslab/bezerra/mcad/ridge/RidgeAgentLearner.java
@@ -0,0 +1,75 @@
+package ch.usi.dslab.bezerra.mcad.ridge;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import ch.usi.dslab.bezerra.netwrapper.Message;
+import ch.usi.dslab.bezerra.ridge.DeliverInterface;
+import ch.usi.dslab.bezerra.ridge.Learner;
+import ch.usi.dslab.bezerra.ridge.RidgeMessage;
+
+public class RidgeAgentLearner implements DeliverInterface {
+   Logger logger = LogManager.getLogger("RidgeAgentLearner");
+   
+   RidgeMulticastAgent rmcAgent;
+   Learner             learner;
+   
+   public RidgeAgentLearner(RidgeMulticastAgent rmcAgent, Learner learner) {
+      this.rmcAgent = rmcAgent;
+      this.learner  = learner;
+      learner.setDeliverInterface(this);
+   }
+   
+   @SuppressWarnings("unchecked")
+   boolean checkIfLocalMessage(RidgeMessage message) {
+      final int localGroupId = rmcAgent.getLocalGroup().getId();
+      List<Integer> destinationGroupIds = (List<Integer>) message.getItem(0);
+      if (destinationGroupIds.contains(localGroupId))
+         return true;
+      else
+         return false;
+   }
+   
+   Message getApplicationMessage(RidgeMessage wrapperMessage) {
+      return (Message) wrapperMessage.getItem(1);
+   }
+
+   @Override
+   public void deliverConservatively(RidgeMessage wrappedMessage) {
+      logger.info("/___ Learner delivered message {} conservatively", wrappedMessage.getId());
+      if (checkIfLocalMessage(wrappedMessage) == false) return;
+      try {
+         rmcAgent.conservativeDeliveryQueue.put(getApplicationMessage(wrappedMessage));
+      } catch (InterruptedException e) {
+         e.printStackTrace();
+         System.exit(1);
+      }
+   }
+
+   @Override
+   public void deliverOptimistically(RidgeMessage wrappedMessage) {
+      logger.info("/___ Learner delivered message {} optimistically", wrappedMessage.getId());
+      if (checkIfLocalMessage(wrappedMessage) == false) return;
+      try {
+         rmcAgent.optimisticDeliveryQueue.put(getApplicationMessage(wrappedMessage));
+      } catch (InterruptedException e) {
+         e.printStackTrace();
+         System.exit(1);
+      }
+   }
+
+   @Override
+   public void deliverFast(RidgeMessage wrappedMessage) {
+      logger.info("/___ Learner delivered message {} fast", wrappedMessage.getId());
+      if (checkIfLocalMessage(wrappedMessage) == false) return;
+      try {
+         rmcAgent.fastDeliveryQueue.put(getApplicationMessage(wrappedMessage));
+      } catch (InterruptedException e) {
+         e.printStackTrace();
+         System.exit(1);
+      }
+   }
+
+}
