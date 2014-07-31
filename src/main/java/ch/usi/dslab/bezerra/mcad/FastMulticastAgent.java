edd4e9c00c0b4b7de27bec99ef5d9fc75afcc4ff
Eduardo Bezerra <eduardo.bezerra@usi.ch>
2014-07-31 11:44:23 +0200

    Minor changes.

--- src/main/java/ch/usi/dslab/bezerra/mcad/FastMulticastAgent.java
+++ src/main/java/ch/usi/dslab/bezerra/mcad/FastMulticastAgent.java
@@ -2,6 +2,6 @@ package ch.usi.dslab.bezerra.mcad;
 
 import ch.usi.dslab.bezerra.netwrapper.Message;
 
-public interface FastMulticastAgent {
+public interface FastMulticastAgent extends MulticastAgent {
    public Message deliverMessageFast();
 }
