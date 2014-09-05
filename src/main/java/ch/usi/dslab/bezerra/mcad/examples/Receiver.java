f64a01b0efb489bca2acca89c1c51e98c73a181b
Eduardo Bezerra <eduardo.bezerra@usi.ch>
2014-09-05 19:23:05 +0200

    Example ready to run.

---
+++ src/main/java/ch/usi/dslab/bezerra/mcad/examples/Receiver.java
@@ -0,0 +1,44 @@
+package ch.usi.dslab.bezerra.mcad.examples;
+
+import ch.usi.dslab.bezerra.mcad.MulticastAgent;
+import ch.usi.dslab.bezerra.mcad.MulticastAgentFactory;
+import ch.usi.dslab.bezerra.netwrapper.Message;
+
+public class Receiver extends Thread {
+   
+   MulticastAgent mcagent;
+   
+   public Receiver(int nodeId, int groupId, String configFile) {
+      mcagent = MulticastAgentFactory.createMulticastAgent(configFile, true, groupId, nodeId);
+   }   
+   
+   @Override
+   public void run() {
+      while (true) {
+         Message msg = mcagent.deliverMessage();
+         System.out.println("delivered message: " + (String) msg.getItem(0));
+      }
+   }
+
+   public static void main(String[] args) {
+      /*
+
+       one of the receivers should start with parameters:  9 1
+         the other receiver should start with parameters: 10 2
+
+       The first parameter is the node id, and the second one is the group id to which the node belongs.
+       Such node must be in the ridge configuration file, under the *group_members* section. This
+       means that (for now) the whole system configuration is static, given in the config file.
+
+      */
+      
+      int nodeId  = Integer.parseInt(args[0]);
+      int groupId = Integer.parseInt(args[1]);
+      String configFile = args[2];
+      //String configFile = "/Users/eduardo/repositories/mine/libmcad/src/main/java/ch/usi/dslab/bezerra/mcad/examples/ridge_2g3e.json";
+      
+      Receiver receiver = new Receiver(nodeId, groupId, configFile);
+      receiver.start();
+   }
+
+}
---
+++ src/main/java/ch/usi/dslab/bezerra/mcad/examples/Sender.java
@@ -0,0 +1,59 @@
+package ch.usi.dslab.bezerra.mcad.examples;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.usi.dslab.bezerra.mcad.Group;
+import ch.usi.dslab.bezerra.mcad.MulticastAgent;
+import ch.usi.dslab.bezerra.mcad.MulticastAgentFactory;
+import ch.usi.dslab.bezerra.netwrapper.Message;
+
+public class Sender {
+   
+   MulticastAgent mcagent;
+   BufferedReader br;
+   
+   public Sender (int senderId, String configFile) {
+      int notUsed = 0;
+      mcagent = MulticastAgentFactory.createMulticastAgent(configFile, false, notUsed, senderId);
+   }
+   
+   public String askForInput() throws IOException {
+      if (br == null) br = new BufferedReader(new InputStreamReader(System.in));
+      System.out.println("multicast message m to g1, to g2 or to g1 & g2. (examples: \"MSG 1 2\", \"m 1\", \"Message 2\"):");
+      String input = br.readLine();
+      return input;
+   }
+   
+   public void sendMessage(String text, List<Group> destinations) {
+      Message multicastMessage = new Message(text);
+      mcagent.multicast(destinations, multicastMessage);
+   }
+   
+   public static void main(String[] args) throws IOException {
+      String configFile = args[0];
+      int    senderId   = Integer.parseInt(args[1]);
+      
+      Sender sender = new Sender(senderId, configFile);
+      
+      String input = sender.askForInput();
+      
+      while (input.equalsIgnoreCase("end") == false) {
+         String[] params = input.split(" ");
+         
+         String message = params[0];
+         List<Group> destinationGroups = new ArrayList<Group>();
+         for (int i = 1 ; i < params.length ; i++) {
+            int groupId = Integer.parseInt(params[i]);
+            destinationGroups.add(Group.getGroup(groupId));
+         }
+         sender.sendMessage(message, destinationGroups);
+         
+         input = sender.askForInput();
+      }
+   }
+
+}
