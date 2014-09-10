6c953214fde83398ef8056e18805bb6a0c1e5df9
Eduardo Bezerra <eduardo.bezerra@usi.ch>
2014-09-10 17:02:08 +0200

    Made example deployment scripts use relative paths.

--- src/main/java/ch/usi/dslab/bezerra/mcad/examples/deployMcast.py
+++ src/main/java/ch/usi/dslab/bezerra/mcad/examples/deployMcast.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 
+import inspect
 import os
 import time
 
-# deploying (for now only ridge) multicast infrastructure
+def script_dir():
+#    returns the module path without the use of __file__.  Requires a function defined 
+#    locally in the module.
+#    from http://stackoverflow.com/questions/729583/getting-file-path-of-imported-module
+   return os.path.dirname(os.path.abspath(inspect.getsourcefile(lambda _: None)))
 
-ridge_deployer = "/Users/eduardo/libmcad/src/main/java/ch/usi/dslab/bezerra/mcad/ridge/RidgeEnsembleNodesDeployer.py "
-config_file = "/Users/eduardo/repositories/mine/libmcad/src/main/java/ch/usi/dslab/bezerra/mcad/examples/ridge_2g3e.json "
+# deploying (for now only ridge) multicast infrastructure
+ridge_deployer = script_dir() + "/../ridge/RidgeEnsembleNodesDeployer.py "
+config_file = script_dir() + "/ridge_2g3e.json "
 
 # to serialize your own app objects, you must add your own classpath to the mcast deployment
 # app_classpath = "/path/to/class/your-app.jar "
