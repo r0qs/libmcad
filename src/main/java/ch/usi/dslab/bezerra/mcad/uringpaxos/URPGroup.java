9b0c4da36348479449cb4f845c6368ab2532ab6d
Eduardo Bezerra <eduardo.bezerra@usi.ch>
2015-02-10 09:56:51 +0100

    Removed unnecessary import.

--- src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/URPGroup.java
+++ src/main/java/ch/usi/dslab/bezerra/mcad/uringpaxos/URPGroup.java
@@ -1,7 +1,6 @@
 package ch.usi.dslab.bezerra.mcad.uringpaxos;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
