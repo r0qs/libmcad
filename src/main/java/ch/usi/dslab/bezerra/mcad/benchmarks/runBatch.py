48b6aea83804e5b8f203a33173d3840635681115
Eduardo Bezerra <eduardo.bezerra@usi.ch>
2015-03-11 14:43:58 +0100

    No message given for this commit.

--- src/main/java/ch/usi/dslab/bezerra/mcad/benchmarks/runBatch.py
+++ src/main/java/ch/usi/dslab/bezerra/mcad/benchmarks/runBatch.py
@@ -36,16 +36,19 @@ maxClients = 70
 incFactor = 1
 incParcel = 10
 #loads = getLoads(minClients, maxClients, incFactor, incParcel)
-loads = [1, 5, 10, 20, 30, 40, 50, 60, 80]
+#loadsGlobal = [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 12, 14, 16, 18, 20, 30, 40, 50, 60, 80, 90, 100, 150, 200 ]
+loadsGlobal = [250,300]
 #numPermits = 1
 ##########################################
 numsLearners = [1, 2, 4, 8, 16, 32]
-#numsLearners = [1, 8, 32]
+#numsLearners = [16, 32]
 ##########################################
 #algorithms = ["libpaxos", "mrp", "ridge"]
-algorithms = ["ridge"]
+algorithms = ["mrp"]
+#algorithms = ["libpaxos"]
 ##########################################
-messageSizes = [200, 8192, 65536]
+#messageSizes = [200, 8192, 65536]
+messageSizes = [200]
 ##########################################
 groups = 0
 pxpergroup = 1
@@ -53,13 +56,13 @@ groupConfigs = [{groups : 1, pxpergroup : 1},
                # {groups : 1, pxpergroup : 2},
                # {groups : 1, pxpergroup : 4},
                # {groups : 1, pxpergroup : 8},
-               {groups : 2, pxpergroup : 1},
-               {groups : 4, pxpergroup : 1},
-               {groups : 8, pxpergroup : 1},
+               # {groups : 2, pxpergroup : 1},
+               # {groups : 4, pxpergroup : 1},
+               # {groups : 8, pxpergroup : 1},
                ]
 ##########################################
-diskConfigs = [False, True]
-#diskConfigs = [False]
+#diskConfigs = [False, True]
+diskConfigs = [False]
 ################################################################################
 ''' clean up environment before running batch
 '''
@@ -68,6 +71,48 @@ time.sleep(5)
 localcmd(systemParamSetter)
 localcmd(clockSynchronizer)
 ################################################################################
+
+loadsDic = {}
+# (nl, ng, size)
+#loadsDic[( 1,1,200  )] = [20,40,60]
+#loadsDic[( 1,1,8192 )] = [12,14]
+#loadsDic[( 1,1,65536)] = [6,7,12,100]
+#loadsDic[( 2,1,200  )] = [90,100,120]
+#loadsDic[( 2,1,8192 )] = [12]
+#loadsDic[( 2,1,65536)] = [7,9,12,100]
+#loadsDic[( 4,1,200  )] = [100,150,200]
+#loadsDic[( 4,1,65536)] = [7,15,100]
+#loadsDic[( 8,1,200  )] = [50,100,120]
+#loadsDic[( 8,1,8192 )] = [6,7,8]
+#loadsDic[( 8,1,65536)] = [12,13,14]
+#loadsDic[(16,1,200  )] = [50,100,120]
+#loadsDic[(16,1,8192 )] = [6]
+#loadsDic[(16,1,65536)] = [100]
+# *** loadsDic[(32,1,200  )] = [200,250,300]
+#loadsDic[(32,1,8192 )] = [7,15,25]
+#loadsDic[(32,1,65536)] = [140,160,180]
+
+# *** loadsDic[( 2,2,200  )] = [150,175]
+#loadsDic[( 2,2,8192 )] = [12,15,17]
+# *** loadsDic[( 2,2,65536)] = [6]
+# *** loadsDic[( 4,2,200  )] = [175,200]
+#loadsDic[( 4,2,8192 )] = [7,9]
+#loadsDic[( 4,2,65536)] = [7,9,100,125,150]
+#loadsDic[( 8,2,200  )] = [100,125,150]
+# *** loadsDic[( 8,2,8192 )] = [4,6]
+#loadsDic[( 8,2,65536)] = [7,15,100,125,150]
+#loadsDic[(16,2,200  )] = [100,125,150]
+#loadsDic[(16,2,8192 )] = [7,9,12,14,16]
+#loadsDic[(16,2,65536)] = [7,9,12,100,125,150]
+# *** loadsDic[(32,2,200  )] = [250,300]
+#loadsDic[(32,2,8192 )] = [6,7,8,9,22,24]
+#loadsDic[(32,2,65536)] = [100,120,140,160,180]
+
+
+# lipaxou
+# (l,g,s)
+#for l in [1,2,4,8] :
+#    loadsDic[(l,1,65536)] = [6,7,8,9]
 
 skips = 0
 if len(sys.argv) > 1 :
@@ -78,6 +123,9 @@ for writeToDisk in diskConfigs :
         for algorithm in algorithms :
             for groupConfig in groupConfigs :
                 for numLearners in numsLearners :
+                    lgs = (numLearners,groupConfig[groups],messageSize)
+                    if loadsDic.has_key(lgs) : loads = loadsDic[lgs]
+                    else                     : loads = loadsGlobal
                     for load in loads :
                         if groupConfig[groups] > numLearners :
                             print "Not running with less learners than groups"
