76192a75a1ad256e0c19355c350d4c44bc18e838
Eduardo Bezerra <eduardo.bezerra@usi.ch>
2015-03-16 14:55:50 +0100

    Added license (LGPL 3) information.

---
+++ src/main/java/ch/usi/dslab/bezerra/mcad/benchmarks/plot_bcast_cdfs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+size=$1
+learners=$2
+outpath=$3
+alg1=$4
+alg1path=$5
+alg2=$6
+alg2path=$7
+alg3=$8
+alg3path=$9
+#alg4=${10}
+#alg4path=${11}
+#maxlat=${12}
+maxlat=${10}
+
+output=${outpath}/cdfs_${learners}_learners_${size}_bytes.ps
+
+gnuplot << END_GNUPLOT
+set terminal postscript eps enhanced color solid lw 2 "Helvetica" 18
+
+set title '$path' #offset 0,-0.5
+
+set xlabel "Latency (ms)"
+set xtics
+set yrange [0:1]
+set xrange [0:${maxlat}*1.5]
+set ylabel
+set ytics
+set ytics add (0.90)
+set ytics add (0.95)
+set ytics add (0.99)
+set grid xtics ytics
+set output "$output"
+
+plot "$alg1path" using ((\$1)/1e6):2 with lines title "$alg1" lc rgb "red"   ,\
+     "$alg2path" using ((\$1)/1e6):2 with lines title "$alg2" lc rgb "green" ,\
+     "$alg3path" using ((\$1)/1e6):2 with lines title "$alg3" lc rgb "blue"  #,\
+#     "$alg4path" using ((\$1)/1e6):2 with lines title "$alg4" lc rgb "black"
+
+END_GNUPLOT
+
+pstopdf $output
+rm $output
--- src/main/java/ch/usi/dslab/bezerra/mcad/benchmarks/plot_broadcast.sh
+++ src/main/java/ch/usi/dslab/bezerra/mcad/benchmarks/plot_broadcast.sh
@@ -4,11 +4,41 @@ directory=$1
 size=$2
 
 input=${directory}/broadcast_${size}.log
-output_tp=${directory}/broadcast_tp_${size}.ps
-output_lat=${directory}/broadcast_lat_${size}.ps
+
+alglinelength=32
+# tp cols
+# 8  -> tp max
+# 15 -> tp 75
+# 22 -> tp power
+# 29 -> tp 1client
+
+datatypes[0]="max"
+tpcols[0]=8
+
+datatypes[1]="75"
+tpcols[1]=15
+
+datatypes[2]="power"
+tpcols[2]=22
+
+datatypes[3]="1client"
+tpcols[3]=29
+
+for i in 0 1 2 3 ;
+do
+
+name=${datatypes[i]}
+output_tp=${directory}/broadcast_tp_${name}_${size}.ps
+output_lat=${directory}/broadcast_lat_${name}_${size}.ps
+
+tpcol=${tpcols[i]}
+let latavgcol=tpcol+1
+let lat95col=tpcol+3
 
 gnuplot << END_GNUPLOT
 set terminal postscript eps enhanced color solid lw 2 "Helvetica" 18
+
+set title "Maximum throughput - ${size}-bytes messages - ${name}"
 
 set xlabel "Destinations"
 set ylabel "Throughput (Mbps)"
@@ -19,11 +49,13 @@ set style histogram cluster gap 1
 set style fill solid border rgb "black"
 set auto x
 
+#set yrange[0:1000]
+
 set output "$output_tp"
 
-plot "$input" using 4 :xtic(1) title column(2 ) fs solid lc rgb "#FFFFFF",\
-    ''        using 10:xtic(1) title column(8 ) fs solid lc rgb "#CCCCCC",\
-    ''        using 16:xtic(1) title column(14) fs solid lc rgb "#999999"
+plot "$input" using $[tpcol+alglinelength*0]:xtic(1) title column(5  ) fs solid lc rgb "#FFFFFF",\
+    ''        using $[tpcol+alglinelength*1]:xtic(1) title column(37 ) fs solid lc rgb "#CCCCCC",\
+    ''        using $[tpcol+alglinelength*2]:xtic(1) title column(69 ) fs solid lc rgb "#999999"
 
 END_GNUPLOT
 
@@ -32,6 +64,8 @@ rm $output_tp
 
 gnuplot << END_GNUPLOT
 set terminal postscript eps enhanced color solid lw 2 "Helvetica" 18
+
+set title "Latency - ${size}-bytes messages - ${name}"
 
 set xlabel "Destinations"
 set ylabel "Latency (ms)"
@@ -42,13 +76,17 @@ set style histogram errorbars gap 2 lw 1
 set style fill solid border rgb "black"
 set auto x
 
+set yrange[0:50]
+
 set output "$output_lat"
 
-plot "$input" using 5 :6 :6 :xtic(1) title column(2 ) fs solid lc rgb "#FFFFFF",\
-    ''        using 11:12:12:xtic(1) title column(8 ) fs solid lc rgb "#CCCCCC",\
-    ''        using 17:18:18:xtic(1) title column(14) fs solid lc rgb "#999999"    
+plot "$input" using $[lat95col+alglinelength*0]:$[latavgcol+alglinelength*0]:$[latavgcol+alglinelength*0]:xtic(1) title column(5 ) fs solid lc rgb "#FFFFFF",\
+    ''        using $[lat95col+alglinelength*1]:$[latavgcol+alglinelength*1]:$[latavgcol+alglinelength*1]:xtic(1) title column(37) fs solid lc rgb "#CCCCCC",\
+    ''        using $[lat95col+alglinelength*2]:$[latavgcol+alglinelength*2]:$[latavgcol+alglinelength*2]:xtic(1) title column(69) fs solid lc rgb "#999999"
 
 END_GNUPLOT
 
 pstopdf $output_lat
 rm $output_lat
+
+done
